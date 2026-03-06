1eae94d8fc885dbef68f0073343010628cef2fc4
mybackpacc-byte <mybackpacc@gmail.com>
2026-03-06 06:54:02 +0000

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,10 +4,22 @@
 
 import { handleAppRequest } from "./app.js";
 import { handleExamRequest } from "./exams.js";
+import { handleBankRequest } from "./question-bank.js";
 
 export async function onRequest(ctx) {
   const url = new URL(ctx.request.url);
   const path = url.pathname;
+
+  // Question bank routes
+  if (
+    path === "/question-bank" ||
+    path === "/qbank-add" ||
+    path === "/qbank-update" ||
+    path === "/qbank-delete" ||
+    path === "/qbank-toggle-visibility"
+  ) {
+    return handleBankRequest(ctx);
+  }
 
   // All exam builder routes go to exams.js
   if (
@@ -17,7 +29,9 @@ export async function onRequest(ctx) {
     path === "/exam-add-question" ||
     path === "/exam-update-question" ||
     path === "/exam-delete-question" ||
-    path === "/exam-reorder-question"
+    path === "/exam-reorder-question" ||
+    path === "/exam-add-from-bank" ||
+    path === "/exam-bank-picker"
   ) {
     return handleExamRequest(ctx);
   }
